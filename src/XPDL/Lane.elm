346a8a0e66ceea6f89d4bdede04ca9620135404a
Eric Butler <idealeric@gmail.com>
2016-10-09 18:26:06 -0400

    Fix Lane Decoder
      * Less Flexible decoding
      * Use listOfOne
      * Use a List instead of a dict so as to not lose ordering

--- src/XPDL/Lane.elm
+++ src/XPDL/Lane.elm
@@ -1,8 +1,8 @@
 module XPDL.Lane exposing (Lanes, Lane, LaneId, lanesDecoder)
 
-import Dict exposing (Dict, fromList)
 import Json.Decode exposing (Decoder, string, list)
-import Json.Decode.Pipeline exposing (decode, nullable, optional)
+import Json.Decode.Pipeline exposing (decode, nullable, optional, required)
+import Json.Decode.XML exposing (listOfOne)
 import XPDL.Performer exposing (Performer, performerDecoder)
 
 
@@ -22,57 +22,29 @@ type alias LaneAttributes =
 
 
 type alias Lanes =
-    Dict LaneId Lane
+    List Lane
 
 
 laneAttrDecoder : Decoder LaneAttributes
 laneAttrDecoder =
-    let
-        stringFromAttrs =
-            Maybe.withDefault ""
-
-        makeAttrs maybeName maybeId =
-            { id = stringFromAttrs maybeId, name = stringFromAttrs maybeName }
-    in
-        decode makeAttrs
-            |> optional "Name" (nullable string) Nothing
-            |> optional "Id" (nullable string) Nothing
+    decode (\id name -> { id = id, name = name })
+        |> required "Id" string
+        |> optional "Name" string ""
 
 
-makeLaneFromDecode : Maybe LaneAttributes -> Maybe (List Performer) -> Lane
-makeLaneFromDecode maybeAttrs maybePerf =
-    let
-        performers =
-            Maybe.withDefault "" (maybePerf `Maybe.andThen` List.head)
-    in
-        case maybeAttrs of
-            Just attrs ->
-                Lane attrs.id attrs.name performers
-
-            _ ->
-                Lane "" "" ""
+makeLaneFromDecode : LaneAttributes -> Performer -> Lane
+makeLaneFromDecode attrs =
+    Lane attrs.id attrs.name
 
 
 laneDecoder : Decoder Lane
 laneDecoder =
     decode makeLaneFromDecode
-        |> optional "$" (nullable laneAttrDecoder) Nothing
-        |> optional "xpdl:Performers" (nullable (list performerDecoder)) Nothing
-
-
-makeLanesFromDecode : Maybe (List Lane) -> Lanes
-makeLanesFromDecode maybeLanes =
-    let
-        realLanes =
-            Maybe.withDefault [] maybeLanes
-
-        lanes =
-            List.map (\l -> ( l.id, l )) realLanes
-    in
-        fromList lanes
+        |> required "$" laneAttrDecoder
+        |> optional "xpdl:Performers" (listOfOne performerDecoder) ""
 
 
 lanesDecoder : Decoder Lanes
 lanesDecoder =
-    decode makeLanesFromDecode
-        |> optional "xpdl:Lane" (nullable (list laneDecoder)) Nothing
+    decode identity
+        |> optional "xpdl:Lane" (list laneDecoder) []
