2bd5f03a667cf053d5834b62c24c3396aa85f896
Eric Butler <idealeric@gmail.com>
2016-11-05 19:23:16 -0400

    Add Activity Offsets

--- src/Visualizer/View.elm
+++ src/Visualizer/View.elm
@@ -3,14 +3,18 @@ module Visualizer.View exposing (visualizer)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import XPDL exposing (..)
-import XPDL.Lane exposing (Lanes, Lane)
+import XPDL.Lane exposing (Lanes, Lane, LaneId)
 import XPDL.Process exposing (Process)
 import XPDL.Activity exposing (Activities, Activity)
-import Dict exposing (get)
+import Dict exposing (Dict, get)
 import State exposing (Msg)
 import Html.CssHelpers exposing (Namespace, withNamespace)
 import Visualizer.Styles exposing (Class(..), namespaceId, activityHeight)
 import Styles.Namespace exposing (FlowstickNamespace)
+
+
+type alias LaneDimensions =
+    { y : Int, height : Int }
 
 
 ns : FlowstickNamespace Class id State.Msg
@@ -18,37 +22,80 @@ ns =
     withNamespace namespaceId
 
 
-laneHtml : String -> Lane -> Html State.Msg
-laneHtml activityBottom lane =
+laneHtml : LaneDimensions -> Lane -> Html State.Msg
+laneHtml laneDims lane =
     div
         [ ns.classList [ ( SystemLane, lane.name == "system" ) ]
-        , style [ ( "height", activityBottom ++ "px" ) ]
+        , style [ ( "height", toString laneDims.height ++ "px" ) ]
         ]
         [ span [] [ text lane.name ] ]
 
 
-activityHtml : Activity -> Html State.Msg
-activityHtml act =
+activityHtml : Dict LaneId LaneDimensions -> Activity -> Html State.Msg
+activityHtml laneDims act =
     let
+        laneDim =
+            getLaneDimensionsWithDefault act.lane laneDims
+
+        thisActY =
+            act.y + laneDim.y
+
         styles =
             style
                 [ ( "left", toString act.x ++ "px" )
-                , ( "top", toString act.y ++ "px" )
+                , ( "top", toString thisActY ++ "px" )
                 ]
     in
         div [ styles ] [ text (Maybe.withDefault "" act.name) ]
 
 
-activitiesHtml : Activities -> Process -> Html State.Msg
-activitiesHtml acts currentProcess =
+activitiesHtml : Dict LaneId LaneDimensions -> Activities -> Process -> Html State.Msg
+activitiesHtml laneDims acts currentProcess =
     let
         actHtml actId =
-            get actId acts |> Maybe.map activityHtml
+            get actId acts |> Maybe.map (activityHtml laneDims)
 
         actHtmls =
             List.map actHtml currentProcess.activities |> List.filterMap identity
     in
         div [ ns.class [ Visualizer.Styles.Activities ] ] actHtmls
+
+
+getLaneDimensionsWithDefault : LaneId -> Dict LaneId LaneDimensions -> LaneDimensions
+getLaneDimensionsWithDefault laneId laneDims =
+    get laneId laneDims
+        |> Maybe.withDefault { y = 0, height = 150 }
+
+
+laneDimensions : List Activity -> List Lane -> Dict LaneId LaneDimensions
+laneDimensions acts lanes =
+    let
+        actMax laneId act accum =
+            if act.lane /= laneId || act.y < accum then
+                accum
+            else
+                act.y
+
+        activityBottom lane =
+            -- Minimum height is starting accum:
+            -- TODO make these contants
+            List.foldr (actMax lane.id) 150 acts
+                |> (+) (activityHeight + 15)
+
+        laneReduction lane accum =
+            let
+                prevLaneY =
+                    List.head accum
+                        |> Maybe.map (\( _, { y, height } ) -> y + height)
+                        |> Maybe.withDefault 0
+
+                item =
+                    ( lane.id, { height = activityBottom lane, y = prevLaneY } )
+            in
+                item :: accum
+    in
+        List.foldr laneReduction [] lanes
+            |> Dict.fromList
 
 
 loadedProcess : XPDLState -> Process -> List (Html State.Msg)
@@ -60,23 +107,14 @@ loadedProcess state process =
         realActs =
             List.filterMap (\lId -> get lId state.activities) process.activities
 
-        reduction laneId act accum =
-            if act.lane /= laneId || act.y < accum then
-                accum
-            else
-                act.y
-
-        activityBottom lane =
-            -- Minimum height is starting accum:
-            List.foldr (reduction lane.id) 150 realActs
-                |> (+) (activityHeight + 15)
-                |> toString
+        laneDims =
+            laneDimensions realActs realLanes
 
         lanesHtml =
-            List.map (\lane -> laneHtml (activityBottom lane) lane) realLanes
+            List.map (\lane -> laneHtml (getLaneDimensionsWithDefault lane.id laneDims) lane) realLanes
     in
         [ div [ ns.class [ Lanes ] ] lanesHtml ]
-            ++ [ activitiesHtml state.activities process ]
+            ++ [ activitiesHtml laneDims state.activities process ]
 
 
 loadedVisualizer : XPDLState -> List (Html State.Msg)
