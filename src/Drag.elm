c563c99e19400836cdfd55926d9a98cfc1f50f13
Eric Butler <idealeric@gmail.com>
2016-11-11 20:24:00 -0500

    Add Drag Model Infrastructure

---
+++ src/Drag.elm
@@ -0,0 +1,88 @@
+module Drag exposing (Msg(..), DragInfo, onClickStartDragging, update, subscriptions, init)
+
+import Mouse exposing (ups, moves)
+import Html exposing (Attribute)
+import Html.Events exposing (defaultOptions, onWithOptions)
+import Json.Decode as Json exposing (Decoder, (:=), map, int, object2)
+
+
+type alias DragInfo =
+    { isDragging : Bool
+    , start : Point
+    , diffX : Int
+    , diffY : Int
+    }
+
+
+type alias Point =
+    ( Int, Int )
+
+
+type Msg
+    = StartDragging Point
+    | StopDragging
+    | Move Point
+
+
+init : DragInfo
+init =
+    { isDragging = False
+    , start = ( 0, 0 )
+    , diffX = 0
+    , diffY = 0
+    }
+
+
+computeDiff : Point -> { x : Int, y : Int } -> Point
+computeDiff start mousePos =
+    ( mousePos.x - fst start
+    , mousePos.y - snd start
+    )
+
+
+update : Msg -> DragInfo -> ( DragInfo, Cmd msg )
+update msg dragInfo =
+    case msg of
+        StartDragging point ->
+            ( { isDragging = True
+              , start = point
+              , diffX = 0
+              , diffY = 0
+              }
+            , Cmd.none
+            )
+
+        StopDragging ->
+            ( init, Cmd.none )
+
+        Move ( newX, newY ) ->
+            -- Check to make sure we're dragging. Sometimes some Move messages
+            -- come after StopDragging has been issued.
+            if dragInfo.isDragging then
+                ( { dragInfo | diffX = newX, diffY = newY }, Cmd.none )
+            else
+                ( dragInfo, Cmd.none )
+
+
+subscriptions : DragInfo -> Sub Msg
+subscriptions dragInfo =
+    if dragInfo.isDragging then
+        Sub.batch
+            [ moves (Move << computeDiff dragInfo.start)
+            , ups (\_ -> StopDragging)
+            ]
+    else
+        Sub.none
+
+
+onClickStartDragging : (Msg -> msg) -> Attribute msg
+onClickStartDragging actionFn =
+    onWithOptions
+        "mousedown"
+        defaultOptions
+        (Json.map (actionFn << StartDragging) decodeMousePosition)
+
+
+decodeMousePosition : Decoder Point
+decodeMousePosition =
+    object2 (,) ("pageX" := int) ("pageY" := int)
--- src/State.elm
+++ src/State.elm
@@ -1,20 +1,24 @@
 module State exposing (Model, Msg(..), init, update, subscriptions)
 
 import XPDL as X
+import Drag
 
 
 type Msg
     = XPDLMsg X.Msg
+    | DragMsg Drag.Msg
 
 
 type alias Model =
     { xpdl : X.XPDL
+    , drag : Drag.DragInfo
     }
 
 
 initialModel : Model
 initialModel =
     { xpdl = X.initialXPDL
+    , drag = Drag.init
     }
 
 
@@ -33,8 +37,17 @@ update msg model =
             in
                 ( { model | xpdl = newXpdl }, xpdlCmd )
 
+        DragMsg dmsg ->
+            let
+                ( newDrag, dragCmd ) =
+                    Drag.update dmsg model.drag
+            in
+                ( { model | drag = newDrag }, dragCmd )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
-        [ Sub.map XPDLMsg (X.subscriptions model.xpdl) ]
+        [ Sub.map XPDLMsg (X.subscriptions model.xpdl)
+        , Sub.map DragMsg (Drag.subscriptions model.drag)
+        ]
